b21c02edd461438cc775f0c26eb87b425a0f972a
nweinshenker <nweinshenker@wisc.edu>
2022-12-01 22:13:36 -0600

    Stupid error

--- advent/src/day1/CommandLineReader.java
+++ advent/src/day1/CommandLineReader.java
@@ -5,35 +5,79 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.PriorityQueue;
 
 public class CommandLineReader {
     public static final String BLANK_CHARACTER = "";
     public static Integer maxValue = Integer.MIN_VALUE;
+    public static Integer maxGroup = Integer.MIN_VALUE;
+    public static PriorityQueue<Integer> pq;
 
     public static void main(String [] args) throws FileNotFoundException {
         BufferedReader reader = new BufferedReader(new FileReader("day1/elf_inventory.txt"));
 
+        pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 < o2) return -1;
+                if (o1 == o2) return 0;
+                return 1;
+            }
+        });
+        
+        ArrayList<Integer> allValues = new ArrayList<>();
         String line = "";
         try {
-            ArrayList<String> calories = new ArrayList<>();
+            ArrayList<String> list = new ArrayList<>();
             while ((line = reader.readLine()) != null) {
-                // System.out.println(line);
+                Integer calories = line.equals(BLANK_CHARACTER) ? 0 : Integer.valueOf(line);
                 if (!line.equals(BLANK_CHARACTER)) {
-                    calories.add(line);
-                } else {
-                    System.out.println(calories.toString());
-                    maxValue = Math.max(largestCaloricContent(calories.toArray(new String [calories.size()])), maxValue);
-                    calories.clear();
+                    list.add(line);
                 }
+                else {
+                    maxValue = Math.max(largestCaloricContent(list.toArray(new String [list.size()])), maxValue);
+                    int sumCalories = 0;
+                    for (int i = 0; i < list.size(); i++) {
+                        sumCalories += Integer.valueOf(list.get(i));
+                    }
+                    allValues.add(sumCalories);
+                    list.clear();
+                }
+
             }
-            System.out.println(maxValue);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
+
+        System.out.printf("Max value %d \n", maxValue);
+        System.out.printf("Max group %d \n", findNLargestCalories(allValues, 3));
+        System.out.printf("Priority queue contents %s \n", pq.toString());
+
+    }
+
+
+    private static Integer findNLargestCalories(ArrayList<Integer> list, int n) {
+    
+        System.out.println(list.size());
+        for (Integer each : list) {
+            pq.add(each);
+            if (pq.size() > n) {
+                pq.poll();
+            }
+        }
+        
+        System.out.println("Queue size" + pq.size());
+        int sum = 0;
+        while (pq.size() > 0) {
+            sum += pq.poll();
+        }
+        return sum;
     }
 
     public static Integer largestCaloricContent(String [] calories) {
